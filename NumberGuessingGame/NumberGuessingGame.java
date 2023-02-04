46a1fa757000c39bc27f0e2e8df2d0cf8b5012c3
SaidMuratOzdemir <114745121+SaidMuratOzdemir@users.noreply.github.com>
2023-02-04 19:15:59 +0300

    Update NumberGuessingGame.java

--- NumberGuessingGame/NumberGuessingGame.java
+++ NumberGuessingGame/NumberGuessingGame.java
@@ -2,61 +2,132 @@ import java.util.Scanner;
 
 public class NumberGuessingGame {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int number = getNumber(input);
-        int guess = getGuess(input);
-        while (guess != number) {
-            int correct = getCorrectDigit(number, guess);
-            int incorrect = getIncorrectDigit(number, guess);
-            System.out.println("Correct place: " + correct);
-            System.out.println("Incorrect place: " + incorrect);
-            guess = getGuess(input);
-        }
-        System.out.println("You guessed the number. Congrats!");
-    }
-
-    public static int getNumber(Scanner input) {
+        Scanner sc = new Scanner(System.in);
+        int number, guess, numberDigitThird, numberDigitSecond, numberDigitFirst, guessDigitThird, guessDigitSecond, guessDigitFirst, incorrectDigit, correctDigit;
+        boolean booleanFirst, booleanSecond, booleanThird;
         System.out.print("Enter a number: ");
-        int number = input.nextInt();
+        number = sc.nextInt();
+
         while (number < 100 || number > 999) {
             System.out.println("Invalid number, enter a three-digit positive number: ");
             System.out.print("Enter a number: ");
-            number = input.nextInt();
+            number = sc.nextInt();
         }
-        return number;
-    }
 
-    public static int getGuess(Scanner input) {
-        System.out.print("Your guess: ");
-        int guess = input.nextInt();
-        while (guess < 100 || guess > 999) {
-            System.out.println("Invalid guess, enter a three-digit positive number: ");
+        do {
             System.out.print("Your guess: ");
-            guess = input.nextInt();
-        }
-        return guess;
-    }
+            guess = sc.nextInt();
 
-    public static int getCorrectDigit(int number, int guess) {
-        int correct = 0;
-        if (number % 10 == guess % 10) correct++;
-        if ((number / 10) % 10 == (guess / 10) % 10) correct++;
-        if (number / 100 == guess / 100) correct++;
-        return correct;
-    }
+            while (guess < 100 || guess > 999) {
+                System.out.println("Invalid guess, enter a three-digit positive number: ");
+                System.out.print("Your guess: ");
+                guess = sc.nextInt();
+            }
+            // We calculate the digits of the number in loop because we need to change them later.
+            numberDigitThird = number % 10;
+            numberDigitSecond = (number / 10) % 10;
+            numberDigitFirst = number / 100;
 
-    public static int getIncorrectDigit(int number, int guess) {
-        int incorrect = 0;
-        int[] num = {number % 10, (number / 10) % 10, number / 100};
-        int[] gue = {guess % 10, (guess / 10) % 10, guess / 100};
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i != j && num[i] == gue[j]) {
-                    incorrect++;
-                    break;
+            guessDigitThird = guess % 10;
+            guessDigitSecond = (guess / 10) % 10;
+            guessDigitFirst = guess / 100;
+
+            incorrectDigit = 0;
+            correctDigit = 0;
+
+            //checking for same numbers for each digit if it is not returns False for each digit
+            if (numberDigitFirst == guessDigitFirst) {
+                correctDigit += 1;
+                booleanFirst = true;
+            } else {
+                booleanFirst = false;
+            }
+
+            if (numberDigitSecond == guessDigitSecond) {
+                correctDigit += 1;
+                booleanSecond = true;
+            } else {
+                booleanSecond = false;
+            }
+
+            if (numberDigitThird == guessDigitThird) {
+                correctDigit += 1;
+                booleanThird = true;
+            } else {
+                booleanThird = false;
+            }
+
+            // booleans are used because the program has to behave differently
+            // this section works when first digit is same, second digit is NOT same and third digit is NOT same
+            if (booleanFirst && !booleanSecond && !booleanThird) {
+                if (numberDigitSecond == guessDigitThird) {
+                    incorrectDigit++;
+                }
+                if (numberDigitThird == guessDigitSecond) {
+                    incorrectDigit++;
                 }
             }
-        }
-        return incorrect;
+
+            // this section works when first digit is NOT same, second digit is same and third digit is NOT same
+            if (!booleanFirst && booleanSecond && !booleanThird) {
+                if (numberDigitFirst == guessDigitThird) {
+                    incorrectDigit++;
+                }
+                if (numberDigitThird == guessDigitFirst) {
+                    incorrectDigit++;
+                }
+            }
+
+            // this section works when first digit is NOT same, second digit is NOT same and third digit is same
+            if (!booleanFirst && !booleanSecond && booleanThird) {
+                if (numberDigitFirst == guessDigitSecond) {
+                    incorrectDigit++;
+                }
+                if (numberDigitSecond == guessDigitFirst) {
+                    incorrectDigit++;
+                }
+            }
+
+            // this section works when first digit is same, second digit is same and third digit is NOT same
+            if (!booleanFirst && !booleanSecond && !booleanThird) {
+                if (guessDigitFirst == numberDigitSecond || guessDigitFirst == numberDigitThird) {
+                    incorrectDigit++;
+
+                    if (guessDigitFirst == numberDigitSecond) {
+                        numberDigitSecond = -1;
+                    } else {
+                        numberDigitThird = -1;
+                    }
+
+                    guessDigitFirst = -1; // this is for avoiding counting the same digit twice
+                }
+
+
+                if (guessDigitSecond == numberDigitFirst || guessDigitSecond == numberDigitThird) {
+                    incorrectDigit++;
+
+                    if (guessDigitSecond == numberDigitFirst) {
+                        numberDigitFirst = -1;
+                    } else {
+                        numberDigitThird = -1;
+                    }
+
+                    guessDigitSecond = -1;
+                }
+
+                if (guessDigitThird == numberDigitFirst || guessDigitThird == numberDigitSecond) {
+                    incorrectDigit++;
+                }
+            }
+            //We don't want to print the result if the user guessed the number
+            if (!(booleanFirst && booleanSecond && booleanThird)) {
+                System.out.println("Correct place: " + correctDigit);
+                System.out.println("Incorrect place: " + incorrectDigit + "\n");
+            }
+
+
+        } while (guess != number);
+
+        System.out.println("You guessed the number. Congrats!");
     }
 }
