745e114a26b97d337f2c5a98a93b8dd803181748
SaidMuratOzdemir <20210808006@ogr.akdeniz.edu.tr>
2023-03-09 14:54:24 +0300

    finished

--- Battleship/TakePosition.java
+++ Battleship/TakePosition.java
@@ -3,6 +3,11 @@ package Battleship;
 import java.util.Scanner;
 
 public class TakePosition {
+    static int firstAbscissa;
+    static int secondAbscissa;
+    static int firstOrdinate;
+    static int secondOrdinate;
+
     public static void main(String[] args){
      int[][] map = new int[10][10];
      String[] shipName = {"Aircraft Carrier","Battleship","Submarine","Cruiser","Destroyer"};
@@ -20,15 +25,112 @@ public class TakePosition {
 
     }
     static String locate(int[][] array,String string, int shipSize){
-        //TODO: check if that position is already taken
+        findLocation(string);
+
+        swap();//swapping helps to avoid repeating code
+
+        if(firstOrdinate==secondOrdinate){ //if ship is horizontal
+            if(secondAbscissa-firstAbscissa==shipSize-1){ //if ship has correct size
+
+                for (int i = 0; i < array[firstOrdinate].length; i++) {//if there is already a ship in that row
+                    if(array[firstOrdinate][i] == 2 || array[firstOrdinate][i] == -1){
+                        if (i<firstAbscissa-1 && i>secondAbscissa-1){//if there is a ship between the two abscissas
+                            return "Error! You placed it too close to another one. Try again:";
+                        }
+                    }
+                }
+
+                for (int i = firstAbscissa; i <= secondAbscissa; i++) {
+
+                    array[firstOrdinate][i-1] = 2;//located successfully
+
+                    // assigning -1 to the cells around the ship because we don't want to place ships next to each other
+                    if(firstOrdinate-1>=0){
+                        array[firstOrdinate-1][i-1] = -1;
+                    }
+                    if(firstOrdinate+1<10){
+                        array[firstOrdinate+1][i-1] = -1;
+                    }
+                }
+                if(firstAbscissa-1-1>=0){
+                    array[firstOrdinate][firstAbscissa-1-1] = -1;
+                }
+                if(secondAbscissa-1+1<10){
+                    array[firstOrdinate][secondAbscissa-1+1] = -1;
+                }
+                return "valid";
+            }
+            else{
+                return "Error! Wrong length of the Submarine! Try again:";
+            }
+        }
+        else if(firstAbscissa==secondAbscissa){
+            if(secondOrdinate-firstOrdinate==shipSize-1){
+
+                for (int i = 0; i < array.length; i++) {//if there is already a ship in that column
+                    if(array[i][firstAbscissa-1] == 2 || array[i][firstAbscissa-1] == -1){
+                        if (i>firstOrdinate-1 && i<secondOrdinate+1){//if there is a ship between the two ordinates
+                            return "Error! You placed it too close to another one. Try again:";
+                        }
+                    }
+                }
+
+                for (int i = firstOrdinate; i <= secondOrdinate; i++) {
+                    array[i][firstAbscissa-1] = 2;//located successfully
+
+                    // assigning -1 to the cells around the ship because we don't want to place ships next to each other
+                    if(firstAbscissa-1-1>=0){
+                        array[i][firstAbscissa-1-1] = -1;
+                    }
+                    if(firstAbscissa-1+1<10){
+                        array[i][firstAbscissa-1+1] = -1;
+                    }
+
+                }
+                if(firstOrdinate-1>=0){
+                    array[firstOrdinate-1][firstAbscissa-1] = -1;
+                }
+                if(secondOrdinate+1<10){
+                    array[secondOrdinate+1][secondAbscissa-1] = -1;
+                }
+                return "valid";
+            }
+            else {
+                return "Error! Wrong length of the Submarine! Try again:";
+            }
+        }
+        else {
+            return "Error! Wrong ship location! Try again:";
+        }
+    }
+
+    static void show(int[][] array){
+        System.out.print("\n  1 2 3 4 5 6 7 8 9 10");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("\n"+ (char)(65+i));
+            for (int j = 0; j < array.length; j++) {
+                if(array[i][j] == 0 || array[i][j] == -1){
+                    System.out.print(" ~"); //empty or unknown
+                }
+                else if(array[i][j] == 1) {
+                    System.out.print(" M"); //missed
+                }
+                else if(array[i][j] == 2) {
+                    System.out.print(" O"); //your ship
+                }
+                else if(array[i][j] == 3) {
+                    System.out.print(" X"); //hitted
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void findLocation(String string) {
         int space = string.indexOf(' ');
 
-        int firstOrdinate = string.charAt(0);
-        int secondOrdinate = string.charAt(space + 1);
-        //TODO: if statement for checking if ordinates are char
-
-        int firstAbscissa = -999;//foo might not be initialized
-        int secondAbscissa = -999;//foo might not be initialized
+        firstOrdinate = string.charAt(0) -65;
+        secondOrdinate = string.charAt(space + 1) -65;
 
         if(space==2){//if first abscissa is one digit
             firstAbscissa = string.charAt(1) - 48;
@@ -49,8 +151,9 @@ public class TakePosition {
             }
 
         }
+    }
 
-        //TODO: if statement for checking if abscissas are number
+    public static void swap() {
         if (firstOrdinate>secondOrdinate){
             int temp = firstOrdinate;
             firstOrdinate = secondOrdinate;
@@ -61,54 +164,6 @@ public class TakePosition {
             firstAbscissa = secondAbscissa;
             secondAbscissa = temp;
         }
-
-
-        if(firstOrdinate==secondOrdinate){
-            if(secondAbscissa-firstAbscissa==shipSize-1){
-                for (int i = firstAbscissa; i <= secondAbscissa; i++) {
-                    array[firstOrdinate-65][i-1] = 2;
-                }
-                return "valid";
-            }
-            else{
-                return "Error! Wrong length of the Submarine! Try again:";
-            }
-        }
-        else if(firstAbscissa==secondAbscissa){
-            if(secondOrdinate-firstOrdinate==shipSize-1){
-                for (int i = firstOrdinate; i <= secondOrdinate; i++) {
-                    array[i-65][firstAbscissa-1] = 2;//success
-                }
-                return "valid";
-            }
-            else {
-                return "Error! Wrong length of the Submarine! Try again:";
-            }
-        }
-        else {
-            return "Error! Wrong ship location! Try again:";
-        }
     }
 
-    static void show(int[][] array){
-        System.out.print("\n  1 2 3 4 5 6 7 8 9 10");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print("\n"+ (char)(65+i));
-            for (int j = 0; j < array.length; j++) {
-                if(array[i][j] == 0){
-                    System.out.print(" ~"); //empty or unknown
-                }
-                else if(array[i][j] == 1) {
-                    System.out.print(" M"); //missed
-                }
-                else if(array[i][j] == 2) {
-                    System.out.print(" O"); //your ship
-                }
-                else if(array[i][j] == 3) {
-                    System.out.print(" X"); //hitted
-                }
-            }
-        }
-        System.out.println();
-    }
 }
