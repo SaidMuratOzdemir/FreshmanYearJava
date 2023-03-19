c673d5db87273fd44f87d5cd4de5c2c0194d14ae
SaidMuratOzdemir <20210808006@ogr.akdeniz.edu.tr>
2023-03-19 16:21:39 +0300

    WordsAndSentences solved

--- ReadabilityScore/WordsAndSentences.java
+++ ReadabilityScore/WordsAndSentences.java
@@ -1,5 +1,30 @@
 package ReadabilityScore;
 
-public class WordsAndSentences {
+import java.util.Scanner;
 
+public class WordsAndSentences {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+
+        int sentenceCount = 0;
+        int wordCount = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if(text.charAt(i) == ' '){
+                wordCount += 1;
+            } else if (text.charAt(i) == '.' || text.charAt(i) == '!' || text.charAt(i) == '?') {
+                sentenceCount += 1;
+                wordCount += 1;
+            }
+        }
+
+        if(text.charAt(text.length() - 1) != '.' && text.charAt(text.length() - 1) != '!' && text.charAt(text.length() - 1) != '?'){
+            sentenceCount += 1;
+            wordCount += 1;
+        }
+
+        System.out.println( (wordCount / sentenceCount) > 10 ? "HARD" : "EASY");
+
+    }
 }
