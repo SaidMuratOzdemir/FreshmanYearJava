900a79216ff48d514a78208c0162fd389009b6f1
SaidMuratOzdemir <20210808006@ogr.akdeniz.edu.tr>
2023-05-27 22:03:13 +0300

    new homeworks and their PDFs

---
+++ Spring/Assignment3/Assignment03_20210808006.java
@@ -0,0 +1,10 @@
+//package Assignment3;
+
+//@author S. Murat. ÖZDEMİR
+//@since 27.05.2023 - 22:01
+
+public class Assignment03_20210808006 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
